fc74e44ba67989f83d5977577e239b4bda7ab690
Joshua Bruylant <joshua.bruylant@ha.ax>
2018-06-26 16:00:38 +0300

    New gpsPlotting version and updated README

--- gpsPlotting/README.md
+++ gpsPlotting/README.md
@@ -2,21 +2,36 @@
 
 ## Setup
 
-In the gmplot folder execute the setup.py install script
+- In the gmplot folder execute the setup.py install script
 
-_Linux_ : 
+_Linux & Mac OS_ : 
 
 ```
 cd gmplot
 sudo python setup.py install
 ```
 
+- You also have to install the pathlib library
 
-Once gmplot is installed execute gpsPlotting_1.0 as follows : 
+_Linux_ :
 
-`python gpsPlotting_1.0.py <dataFile>`
+`pip install pathlib` or use your usual installer
 
-Replace `<dataFile>` with the name of your csv file containing latitude and longitude points of ASPire's route
+_Mac OS_ :
+
+`easy_install pathlib` or use your usual installer
+
+**-----**
+
+Once gmplot is installed execute gpsPlotting_1.1 as follows : 
+
+`python gpsPlotting_1.1.py <csv File> <Interval between each timestamp marker>`
+
+- Replace `<csv File>` with the name of your csv file containing latitude and longitude points of ASPire's route
+**N.B.:** This csv file must contain the columns time or t_timestamp, latitude, longitude and satellites_used (Use my other script, *logExtraction* in order to easily create this csv file)
+
+- Replace `<Interval between each timestamp marker>` with the amount of points you want to skip between placing each timestamp marker.
+**Warning** : The lower the interval, the more clustered the map will be. Having the interval too low (<20 is risky) has a high chance of making the map crash all together.
 
 
 The script will then plot all points and create a route between them on a google map named ASPire_route.html
---
+++ gpsPlotting/gpsPlotting_1.1.py
@@ -0,0 +1,105 @@
+import sys
+import csv
+import numpy as np
+from gmplot import gmplot
+from pathlib import Path
+
+"""
+ASPire GPS route plotting script
+Author : Joshua Bruylant
+Date : 26 June 2018
+Version 1.1
+
+
+Description : 
+Extracts latitude and longitude information of ASPire's route and plots them to a google map
+Requires a log file containing latitude, longitude and satellites_used to be passed as argument when executing the script
+
+HOW TO EXECUTE : python gpsPlotting_1.1.py <Path to csv file> <Interval between each timestamp marker>
+N.B.: The lower the interval, the more precise but the more clustered the markers are. Too low may make the map crash because of the density
+"""
+
+expectedArgs = 3
+
+#Argument detection and handling
+if (len(sys.argv) > expectedArgs): #Too many arguments
+	sys.exit("Too many arguments! Please give following arguments : <csv file name> <Marker interval>")
+if (len(sys.argv) == expectedArgs): #Right amount of arguments
+	logFile = sys.argv[1]
+	logPathFile = Path(logFile)
+	if not logPathFile.is_file():
+		sys.exit("Log file doesn't exist or can't be found!")
+	
+	markerInterval = int(sys.argv[2])
+	#Interval between each marker very low, warn user
+	if markerInterval<=35: 
+		continueFlag = 0
+		while continueFlag == 0:
+			check = input("WARNING! Such low interval will result in a very high marker density and things may go wrong! Are you sure? [y/N] : ") or 'no' #defaults to no
+			if (check.lower() in ['n', 'no']) :
+				continueFlag = 1
+				sys.exit("You chose the safe way out")
+			elif check.lower() in ['y', 'yes']:
+				continueFlag = 1
+			else : 
+				continueFlag = 0
+		
+if (len(sys.argv) < expectedArgs): #Not enough arguments
+	sys.exit("Not enough arguments. Required : <csv file name> <Marker interval>")
+		
+		 
+
+times=[]	#list containing all timestamps parsed from csv file
+lats=[]		#list containing all latitudes
+longs=[]	#list of longitudes
+sats=[]		#list of all satellites used for each lat/long
+minSatellitesThreshold = 6 #Minimum satellites needed in order to trust the data
+mapFile = 'ASPire_route.html' #File to write to when drawing map
+
+
+#Extract lattitudes and longitudes from logs.csv
+with open(logFile, 'r') as csvfile:
+	reader = csv.reader(csvfile)
+	firstLine = next(reader)
+	#Check each header column to determine where the latitudes and longitudes are situated
+	for n in range(len(firstLine)):
+		if (firstLine[n] in ['time', 't_timestamp']) : timeCol = n		
+			
+		if (firstLine[n] == 'latitude') : latCol = n 
+			
+		if (firstLine[n] == 'longitude') : longCol = n 
+		
+		if (firstLine[n] == 'satellites_used') : satCol = n 
+			
+	if not latCol : sys.exit("No Latitude!")	
+	if not longCol : sys.exit("No longitude!")
+	
+	#Add lats and longs to their list
+	for row in reader:
+		numberOfSatellites = int(row[satCol])
+		if not numberOfSatellites <= minSatellitesThreshold : #Add only points with enough satellites used
+			times.append(row[timeCol]) 
+			lats.append(float(row[latCol]))
+			longs.append(float(row[longCol]))
+			sats.append(int(row[satCol]))
+
+
+#Calculate mean latitude and longitude to center map
+meanLat = np.mean(lats)
+meanLong = np.mean(longs)
+
+# Place map
+gmap = gmplot.GoogleMapPlotter(meanLat, meanLong, 11)
+
+#Plot route
+gmap.plot(lats, longs, '#2980b9', edge_width=5)
+
+#Place timestamp markers
+for n in range(len(times)):
+	if not (n%markerInterval) :
+		gmap.marker(lats[n], longs[n], color = 'lightsalmon', title=times[n])
+
+#Draw map
+gmap.draw(mapFile)
+
+print("Map creation successful : " + mapFile)
